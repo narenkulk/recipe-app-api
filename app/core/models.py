7a2ee6ceb49c4cafbb45c6e1908283e2352f0d70
narenkulk <66332810+narenkulk@users.noreply.github.com>
2020-06-13 20:56:23 -0400

    Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+#Modify the User model
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
