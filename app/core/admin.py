51c971b3d5429b18473a3f1503b57f12a6a18980
narenkulk <naren.kulk@gmail.com>
2020-06-13 22:21:41 -0400

    comment.

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from . import models
-# user admin class
+# user admin class.
 
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
 
-#Register the site
+
+# Register the site
 admin.site.register(models.User, UserAdmin)
